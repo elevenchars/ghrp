d16f31bf8328d768559a1d0f05e52b04347fd977
Will Ingarfield <will.ingarfield@gmail.com>
2019-04-23 18:53:59 -0500

    Various formatting changes, github oauth app support

--- test.py
+++ test.py
@@ -13,7 +13,9 @@ def get_newest_push(events: dict):
 
 config = {
     "github_username": None,
-    "discord_client_id": None
+    "discord_client_id": None,
+    "github_client_id": None,
+    "github_client_secret": None,
 }
 
 config_location = "config.json"
@@ -34,10 +36,20 @@ rpc.connect()
 
 sess = requests.Session()
 headers = {}
+payload = None
+if config["github_client_id" and "github_client_secret"]:
+    payload = {
+        "client_id": config["github_client_id"],
+        "client_secret": config["github_client_secret"]
+    }
+else:
+    print("Proceeding without GitHub OAuth (60 rq/hr)")
 
+events_url = "https://api.github.com/users/{}/events".format(config["github_username"])
 # rpc.update(join="fffffff")
 while True:
-    events_rq = sess.get("https://api.github.com/users/{}/events".format(config["github_username"]), headers=headers, timeout=10)
+    # this crashes :( don't know why
+    events_rq = sess.get(events_url, headers=headers, params=payload)
     if events_rq.status_code != 304:
         headers["If-None-Match"] = events_rq.headers["ETag"]
         events = json.loads(events_rq.text)
@@ -50,6 +62,6 @@ while True:
     # the split is so that we only get the tagline (?) of the repo
     commit_message = latest["payload"]["commits"][0]["message"].split("\n")[0]
     print("Presence Updated\nRepo: {}\nMessage: {}".format(repo_name, commit_message))
-    rpc.update(details=repo_name, state=commit_message)
+    rpc.update(details=repo_name, state=commit_message, large_image="github")
     time.sleep(60)  # can be changed, this is just so that ratelimit doesn't
     # get roasted
